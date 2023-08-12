2f5b7c81e0f675320cb158d1bcce60be5ea9644f
Jesse Spielman <jesse.spielman@gmail.com>
2023-08-12 02:59:54 +0100

    Add first pass of MPD script...

---
+++ ct/mpd.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2023 heavyimage
+# Author: heavyimage
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+                                                 
+     ______  _______        _____        _____   
+    |      \/       \   ___|\    \   ___|\    \  
+   /          /\     \ |    |\    \ |    |\    \ 
+  /     /\   / /\     ||    | |    ||    | |    |
+ /     /\ \_/ / /    /||    |/____/||    | |    |
+|     |  \|_|/ /    / ||    ||    |||    | |    |
+|     |       |    |  ||    ||____|/|    | |    |
+|\____\       |____|  /|____|       |____|/____/|
+| |    |      |    | / |    |       |    /    | |
+ \|____|      |____|/  |____|       |____|____|/ 
+    \(          )/       \(           \(    )/   
+     '          '         '            '    '    
+                                                 
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="mpd"
+var_disk="16"
+var_cpu="1"
+var_ram="512"
+var_os="debian"
+var_version="11"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /usr/bin/mpd ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Stopping mpd"
+systemctl stop mpd.service
+msg_ok "Stopped mpd"
+
+msg_info "Updating mpd"
+apt upgrade mpd mpc
+msg_ok "Updated mpd"
+
+msg_info "Starting mpd"
+systemctl start mpd.service
+msg_ok "Started mpd"
+
+msg_ok "Updated Successfully"
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
---
+++ install/mpd-install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2023 heavyimage
+# Author: heavyimage
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+
+msg_info "Installing mpd"
+$STD apt-get install -y mpd mpc
+msg_ok "Installed mpd"
+
+msg_info "Disabling user service"
+systemctl --user disable mpd
+msg_info "Disabled user service"
+
+msg_info "disabling system service for installation"
+systemctl disable mpd
+msg_info "disabled system service for installation"
+
+
+msg_info "Creating config file"
+cat <<EOF >/etc/mpd.conf
+music_directory    "/mnt/music/""
+bind_to_address    "0.0.0.0"
+
+database {
+    plugin           "simple"
+    path             "/var/lib/mpd/mpd.db"
+    cache_directory  "/var/lib/mpd/cache"
+}
+
+EOF
+
+msg_info "enabling system service"
+systemctl enable mpd
+msg_info "enabled system service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
