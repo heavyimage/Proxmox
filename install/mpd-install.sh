9e4ad4f3112dcd06697708c0509c437767ae7f03
Jesse Spielman <jesse.spielman@gmail.com>
2023-08-12 04:24:59 +0100

    Directly mount music

--- install/mpd-install.sh
+++ install/mpd-install.sh
@@ -24,7 +24,7 @@ msg_info "disabled system service for installation"
 
 msg_info "Creating config file"
 cat <<EOF >/etc/mpd.conf
-music_directory    "/mnt/media/music/music"
+music_directory    "/mnt/music"
 bind_to_address    "0.0.0.0"
 
 database {
@@ -38,13 +38,14 @@ msg_info "Created config file"
 
 msg_info "Adding mountpoint"
 cat <<EOF >>/etc/fstab
-192.168.0.44:/volume1/media      /mnt/media      nfs  vers=4  0  0
+192.168.0.44:/volume1/media/music/music      /mnt/music      nfs  vers=4  0  0
 EOF
 msg_info "Added mountpoint"
 
-msg_info "Mounting media"
+msg_info "Mounting music"
+mkdir -p /mnt/music
 mount -a
-msg_info "Mounted media"
+msg_info "Mounted music"
 
 msg_info "enabling system service"
 systemctl enable mpd
